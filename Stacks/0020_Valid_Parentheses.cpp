6d8f77f4ed5cc1522bceb3cf071a2cf8d67bd0cf
riavinod10 <riavinodk@gmail.com>
2026-06-16 15:39:29 +0530

    lc 20 added

---
+++ Stacks/0020_Valid_Parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char ch:s){
+            if(ch=='(' || ch=='{' || ch=='[') st.push(ch);
+            else{
+                if(st.empty()) return false;
+                else if(st.top()!='(' && ch==')' ||
+                        st.top()!='{' && ch=='}' ||
+                        st.top()!='[' && ch==']') return false;
+                else st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
